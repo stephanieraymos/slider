abe18af5ff16be6232efe36ae9fd621779009bfb
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-22 13:26:40 -0800

    Previous + next buttons

--- src/App.js
+++ src/App.js
@@ -26,6 +26,12 @@ function App() {
             </article>
           );
         })}
+        <button className="prev">
+          <FiChevronLeft />
+        </button>
+        <button className="next">
+          <FiChevronRight />
+        </button>
       </div>
     </section>
   );
