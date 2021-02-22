43220c2141336dfe2cde0a07925efc42af5e64a6
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-22 13:19:27 -0800

    Project baseplate + state

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+function App() {
+    const [people, setPeople] = useState(data);
+    const [index, setIndex] = useState(0);
+    
+  return <></>;
+}
+
+export default App;
